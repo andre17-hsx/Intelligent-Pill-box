0a1dcbc6a1b583f273fd91b8941ca8f3de127c9d
Karla Moscoso Arrieta <kamoarri@espol.edu.ec>
2022-01-03 20:52:39 -0500

    "Activity 4 para crear cuenta, activación botón CrearCuenta en Activity 2"

--- Proy_G3_PastilleroInteligente/app/src/main/java/com/example/proy_g3_pastillerointeligente/MainActivity2.java
+++ Proy_G3_PastilleroInteligente/app/src/main/java/com/example/proy_g3_pastillerointeligente/MainActivity2.java
@@ -2,13 +2,27 @@ package com.example.proy_g3_pastillerointeligente;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+    Button botonCC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        botonCC=(Button)findViewById(R.id.botonCC);
+
+        botonCC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i2 = new Intent(MainActivity2.this, MainActivity4.class);
+                startActivity(i2);
+            }
+        });
     }
 }
