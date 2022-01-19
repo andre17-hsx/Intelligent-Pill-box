0da6a5a76ff3e331eb6ab79430e7820ff7a8e9e7
Kabir Canarte <kcanarte@espol.edu.ec>
2022-01-19 17:17:33 -0500

    Se creo la base de datos remota y la validacion de usuario

--- Proy_G3_PastilleroInteligente/app/src/main/java/com/example/proy_g3_pastillerointeligente/DataBase.java
+++ Proy_G3_PastilleroInteligente/app/src/main/java/com/example/proy_g3_pastillerointeligente/DataBase.java
@@ -1,16 +1,34 @@
 package com.example.proy_g3_pastillerointeligente;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class DataBase extends AppCompatActivity {
     private EditText etName,etID,etEmail,etPassword;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+    DatabaseReference mRootReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,24 +39,81 @@ public class DataBase extends AppCompatActivity {
         etEmail=(EditText)findViewById(R.id.textEmail);
         etPassword=(EditText)findViewById(R.id.textPassword);
 
+        //Inicializar objeto Firebase para conexion con base de datos
+        firebaseAuth= FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+
+
     }
     public void Registrar(View v) {
-        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "administracion", null, 1);
-        SQLiteDatabase bd = admin.getWritableDatabase();
-        String name = etName.getText().toString();
-        String id = etID.getText().toString();
-        String email = etEmail.getText().toString();
-        String password = etPassword.getText().toString();
-        bd.execSQL("insert into usuarios (name,ID,email,password) values ("+"'"+name+"',"+id+",'"+email+"',"+"'"+password+"')");
-        //insert into articulos (codigo,descripcion,precio) values (1, raspberrypi3, 65.39)
-        bd.close();
-        etName.setText("");
-        etID.setText("");
-        etEmail.setText("");
-        etPassword.setText("");
-        Toast.makeText(this, "Se cargaron los datos del usuario",
-                Toast.LENGTH_SHORT).show();
-    }
 
+
+        String name = etName.getText().toString().trim();
+        String id = etID.getText().toString().trim();
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(name)){
+            Toast.makeText(this, "Se  debe ingresar un name",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(id)){
+            Toast.makeText(this, "Se  debe ingresar un id",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Se  debe ingresar un email",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Se  debe ingresar un password",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        progressDialog.setMessage("Realizando registro en linea...");
+        progressDialog.show();
+
+
+
+
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //checking if success
+                        if (task.isSuccessful()){
+                            mRootReference = FirebaseDatabase.getInstance().getReference("users");
+                            Toast.makeText(DataBase.this, "Usuario registrado", Toast.LENGTH_SHORT).show();
+                            //Iniciarlizar el objeto Map y obtener datos del usuario
+                            Map<String,Object> datosUsuario = new HashMap<>();
+                            Map<String,Object> Pastillas = new HashMap<>();
+
+                            datosUsuario.put("name", name);
+                            datosUsuario.put("contrasena", password);
+
+                            //crear lista de pastillas del usuario
+                            datosUsuario.put("notificacion",Pastillas);
+
+                            //subir informacion a la base de datos
+                            mRootReference.child(id).setValue(datosUsuario);
+
+
+
+
+                        }else{
+                            if(task.getException() instanceof FirebaseAuthUserCollisionException){
+                                Toast.makeText(DataBase.this, "Usuario ya registrado, ingrese nuevamente", Toast.LENGTH_SHORT).show();
+
+                            }else{
+                                Toast.makeText(DataBase.this, "No se pudo registrar usuario", Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                        progressDialog.dismiss();
+
+                    }
+                });
+    }
 
 }
--- Proy_G3_PastilleroInteligente/app/src/main/java/com/example/proy_g3_pastillerointeligente/MainActivity2.java
+++ Proy_G3_PastilleroInteligente/app/src/main/java/com/example/proy_g3_pastillerointeligente/MainActivity2.java
@@ -1,26 +1,45 @@
 package com.example.proy_g3_pastillerointeligente;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
 public class MainActivity2 extends AppCompatActivity {
     private EditText textUser, textPass;
+
+    //Imagen de cargando y declarar  un objeto firebaseAuth.
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        //Datos del usuario
         textUser=(EditText)findViewById(R.id.textUser);
         textPass=(EditText)findViewById(R.id.textPass);
 
+        //Inicializar objeto Firebase para conexion con base de datos
+        firebaseAuth= FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
     }
     public void register(View view) {
         Intent i = new Intent(this, DataBase.class );
@@ -29,27 +48,38 @@ public class MainActivity2 extends AppCompatActivity {
 
     public void Loggin(View v){
 
-        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,
-                "administracion", null, 1);
-        SQLiteDatabase bd = admin.getReadableDatabase();
-        String id = textUser.getText().toString();
-        String pass = textPass.getText().toString();
+        String email = textUser.getText().toString().trim();
+        String password = textPass.getText().toString().trim();
 
-        Cursor user = bd.rawQuery(
-                "select * from usuarios where ID=" + id,
-                null);
-        Cursor password = bd.rawQuery(
-                "select * from usuarios where password=" + "'"+pass+"'",
-                null);
-        if (user.moveToFirst() && password.moveToFirst()) {
-            Intent i = new Intent(this, PillRegister.class );
-            i.putExtra("User", textUser.getText().toString());
-            i.putExtra("Password", textPass.getText().toString());
-            startActivity(i);
-        } else
-            Toast.makeText(this, "Datos incorrectos, verifique su usuario o contraseña",
-                    Toast.LENGTH_SHORT).show();
-        bd.close();
 
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Se  debe ingresar un id",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Se  debe ingresar un password",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        progressDialog.setMessage("Iniciando sesion...");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        //checking if success
+                        if (task.isSuccessful()){
+                            Toast.makeText(MainActivity2.this, "BIENVENIDO", Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(getApplication(), Menu.class );
+                            startActivity(i);
+
+                        }else{
+                            Toast.makeText(MainActivity2.this, "Datos no coinciden con ningun usuario registrado", Toast.LENGTH_LONG).show();
+                        }
+                        progressDialog.dismiss();
+
+                    }
+                });
     }
 }
